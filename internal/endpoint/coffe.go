720c3d0e93ae10befa1bf5eb8f7b9635fde917d1
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-03 20:19:42 +0100

    Update coffee.go

--- internal/endpoint/coffe.go
+++ internal/endpoint/coffe.go
@@ -7,5 +7,5 @@ import (
 
 func HandlerCoffee(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Host, "wants coffee, but i'm a teapot!")
-	http.Error(w, "I'm a teapot!", http.StatusTeapot)
+	http.Error(w, "I cannot brew coffee, for I am a teapot!", http.StatusTeapot)
 }
