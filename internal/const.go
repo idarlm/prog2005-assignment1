13484072a0ae02262d17604364f0629867c5ee11
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-03 20:18:41 +0100

    update path constants

--- internal/const.go
+++ internal/const.go
@@ -6,11 +6,11 @@ const DEFAULT_PORT = "8080"
 
 // Version root paths
 
-const ROOT_PATH_V1 = "/unisearcher/v1/"
+const ROOT_PATH_V1 = "/unisearcher/v1"
 
 // Endpoint branch paths
 
-const UNIINFO_PATH = "uniinfo/"
-const NEIGHBOURUNI_PATH = "neighbourunis/"
-const DIAG_PATH = "diag"
-const COFFEE_PATH = "coffee" // EXTREMELY IMPORTANT!
+const UNIINFO_PATH = "/uniinfo"
+const NEIGHBOURUNI_PATH = "/neighbourunis"
+const DIAG_PATH = "/diag"
+const COFFEE_PATH = "/coffee" // EXTREMELY IMPORTANT!
