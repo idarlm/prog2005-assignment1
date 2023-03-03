1e284e0729044cdab06a866fe023e77f0452dbb0
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-03 20:53:10 +0100

    Rename print function

--- internal/endpoints.go
+++ internal/endpoints.go
@@ -9,15 +9,15 @@ import (
 func ListenCoffee() {
 	path := COFFEE_PATH
 	http.HandleFunc(path, endpoint.HandlerCoffee)
-	printListener(path)
+	printEndpoint(path)
 }
 
 func ListenDiag() {
 	path := ROOT_PATH_V1 + DIAG_PATH
 	http.HandleFunc(path, endpoint.HandlerDiag)
-	printListener(path)
+	printEndpoint(path)
 }
 
-func printListener(path string) {
-	fmt.Println("Set endpoint:", path)
+func printEndpoint(path string) {
+	fmt.Println("Active endpoint:", path)
 }
