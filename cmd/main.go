5a1e06d01288121339f36db124d5e83214eb772a
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-03 08:55:49 +0100

    Define basic project structure

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	server "assignment1/internal/server"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("Default port:", server.DEFAULT_PORT)
+}
--- internal/server/const.go
+++ internal/server/const.go
@@ -1,4 +1,4 @@
-package main
+package server
 
 // Server properties
 const DEFAULT_PORT = 8080
