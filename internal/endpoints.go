f4ab1b313106b67de09fd64fd519f63162ab2dd7
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-04 15:59:52 +0100

    Delete endpoint.go

--- internal/endpoints.go
+++ internal/endpoints.go
@@ -6,15 +6,31 @@ import (
 	"net/http"
 )
 
+// Assign handler func to path
 func HandlePath(path string, handler func(w http.ResponseWriter, r *http.Request)) {
 	http.HandleFunc(path, handler)
-	fmt.Println("Set endpoint:" + path)
+	fmt.Println("Set endpoint:", path)
 }
 
+// Only allow GET METHOD for handler
+func HandleGET(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Only GET method is allowed.", http.StatusMethodNotAllowed)
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
+// set handler for coffee
 func HandleCoffee() {
-	HandlePath(COFFEE_PATH, endpoint.HandleGET(endpoint.CoffeeHandler))
+	HandlePath(COFFEE_PATH, HandleGET(endpoint.CoffeeHandler))
 }
 
+// set handler for
 func HandleDiag() {
-	HandlePath(ROOT_PATH_V1+DIAG_PATH, endpoint.HandleGET(endpoint.DiagHandler))
+	HandlePath(ROOT_PATH_V1+DIAG_PATH, HandleGET(endpoint.DiagHandler))
 }
