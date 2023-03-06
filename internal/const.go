a3d894e83b4e9e72b103715c4ae3cc8aff4c2d40
Idar Mykløy <idarmy@stud.ntnu.no>
2023-03-06 01:28:13 +0100

    Update const.go

--- internal/const.go
+++ internal/const.go
@@ -10,7 +10,10 @@ const ROOT_PATH_V1 = "/unisearcher/v1"
 
 // Endpoint branch paths
 
-const UNIINFO_PATH = "/uniinfo"
-const NEIGHBOURUNI_PATH = "/neighbourunis"
-const DIAG_PATH = "/diag"
+const (
+	UNIINFO_PATH      = "/uniinfo/"       // Path component for uniinfo endpoint
+	NEIGHBOURUNI_PATH = "/neighbourunis/" // Path component for neighbourunis endpoint
+	DIAG_PATH         = "/diag/"          // Path component for diag endpoint
+)
+
 const COFFEE_PATH = "/coffee" // EXTREMELY IMPORTANT!
